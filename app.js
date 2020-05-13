739d3aff0fa7297fce5479d9b54dd20d6972a772
Johannes Galanjuk <johannes.galanjuk@brandseven.de>
2020-05-13 16:12:39 +0200

    added more classes and hierarchy

--- app.js
+++ app.js
@@ -11,15 +11,53 @@ app.use('/client', express.static(__dirname + '/client'));
 server.listen(3000);
 console.log("Server started");
 
-var io = require('socket.io')(server,{});
+var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket) {
 	console.log('socket connected');
 
-	socket.on('happy', function(data) {
-		console.log('happy because ' + data.reason);
+	socket.on('updatePosition', function(data) {
+		Player.position.x = data.x;
+		Player.position.y = data.y;
 	});
 
 	socket.emit('serverMsg', {
 		msg:'hello',
 	});
 });
+
+
+
+
+
+class Entity {
+	constructor(position, direction) {
+		this.position = position;
+		this.direction = direction;
+	}
+}
+
+class Player extends Entity {
+	constructor(id, position, direction) {
+		super(position, direction);
+	}
+
+	move(x, y) {
+		this.position.x = x;
+		this.position.y = y;
+	}
+
+	update() {
+
+	}
+}
+
+class Projectile extends Entity {
+	constructor(position, direction) {
+		super(position, direction);
+	}
+}
+
+class Weapon {
+	constructor() {
+	}
+}
