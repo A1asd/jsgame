56392fc1f3a09fc6c6f7ecc4a399d4a9d5d1d0bd
Johannes Galanjuk <johannes.galanjuk@brandseven.de>
2020-06-04 15:55:36 +0200

    restructured client side a little bit

--- client/Entities.js
+++ client/Entities.js
@@ -7,6 +7,15 @@ class Player {
 		this.hp = initPackage.hp;
 		this.maxHp = initPackage.maxHp;
 		this.score = initPackage.score;
+
+		switch (this.id) {
+			case 0: this.color = "red"; break;
+			case 1: this.color = "green"; break;
+			case 2: this.color = "blue"; break;
+			case 3: this.color = "yellow"; break;
+			default: this.color = "white";
+		}
+
 		Player.list[this.id] = this;
 	}
 	static list = {};
@@ -17,6 +26,7 @@ class Projectile {
 		this.id = initPackage.id;
 		this.x = initPackage.x;
 		this.y = initPackage.y;
+
 		Projectile.list[this.id] = this;
 	}
 	static list = {};
@@ -25,5 +35,6 @@ class Projectile {
 class Boss {
 	constructor(initPackage) {
 	}
+
 	static list = {};
 }
--- client/Renderer.js
+++ client/Renderer.js
@@ -7,51 +7,66 @@ class Renderer {
 		this.config = config;
 	}
 
-	drawPlayerSprite(player) {
+	drawPlayerSprite(player) { //TODO: add animation (e.g. ++currentFrame, Animationclass, etc.)
 		ctx.globalCompositeOperation = "source-over";
+
+		let img = new Image();
+		img.src = player.sprite.src;
+
+		let dArr = [-1,-1, 0,-1, 1,-1, -1,0, 1,0, -1,1, 0,1, 1,1], // offset array
+			s = this.config.outlineThickness;
+
+		for (let i = 0; i < dArr.length; i += 2)
+			ctx.drawImage(img.src, player.x + dArr[i]*s, player.y + dArr[i+1]*s, 30, 30);
+
+		// fill with color
+		ctx.globalCompositeOperation = "source-in";
+		ctx.fillStyle = player.color;
+		ctx.fillRect(0, 0 ,this.config.clientWidth, this.config.clientHeight);
+
+		// draw original image in normal mode
+		ctx.globalCompositeOperation = "source-over";
+		ctx.drawImage(img.src, player.x, player.y, 30, 30);
+	}
+
+	drawMap(map) {
+		ctx.globalCompositeOperation = "source-over";
+	}
+
+	drawUi() {
+		//outline demo:
+		let hpWidth = 200 * player.hp / player.maxHp; //TODO: make this work!
+
+		//ctx.fillRect(x - hpWidth / 2, y - 40, hpWidth, 4);
+		hpBar.style.width = hpWidth;
+		ctx.fillText(player.score, player.x, player.y - 60);
 	}
 
 	clearScreen() {
 		ctx.clearRect(0, 0, this.config.clientWidth, this.config.clientHeight);
 	}
 
-	draw(player, x, y) {
-		//outline demo:
-		let img = new Image();
-
-		img.src = player.sprite.src;
-
-		let hpWidth = 200 * player.hp / player.maxHp;
-
-		//ctx.fillRect(x - hpWidth / 2, y - 40, hpWidth, 4);
-		hpBar.style.width = hpWidth;
-		ctx.fillText(player.score, x, y - 60);
-
-		let dArr = [-1,-1, 0,-1, 1,-1, -1,0, 1,0, -1,1, 0,1, 1,1], // offset array
-			s = this.config.outlineThickness;
+	draw() {
+		this.drawMap(map)
 
 		// draw images at offsets from the array scaled by s
-		for (let i = 0; i < dArr.length; i += 2)
-			ctx.drawImage(img, x + dArr[i]*s, y + dArr[i+1]*s, 30, 30);
+		for (let i in Player.list) {
+			this.drawPlayerSprite(Player.list[i]);
+		}
 
-		// fill with color
-		ctx.globalCompositeOperation = "source-in";
-		ctx.fillStyle = "red";
-		ctx.fillRect(0, 0 ,this.config.clientWidth, this.config.clientHeight);
-
-		// draw original image in normal mode
-		ctx.globalCompositeOperation = "source-over";
-		ctx.drawImage(img, x, y, 30, 30);
+		this.drawUi();
 	}
 }
 
 setInterval(function() {
 	renderer.clearScreen();
-	for (let i in Player.list) {
-		renderer.draw(Player.list[i], Player.list[i].x, Player.list[i].y);
-	}
+
+	renderer.draw();
+
 	ctx.fillStyle = "black";
 	for (let i in Projectile.list) {
 		ctx.fillRect(Projectile.list[i].x-2, Projectile.list[i].y-2, 4, 4);
 	}
+
+	renderer.drawUi();
 }, 40);
